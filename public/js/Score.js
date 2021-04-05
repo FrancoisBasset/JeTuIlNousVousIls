639ee07da073439d2b99cff3445045482aa4abd8
FrancoisBasset <>
2021-04-05 14:59:28 +0200

    maj

--- public/js/Score.js
+++ public/js/Score.js
@@ -16,5 +16,11 @@ class Score {
 
 	updateTense(index, number) {
 		this.#element.rows[index].cells[1].innerText = number;
+
+		if (number[0] == number[4]) {
+			this.#element.rows[index].className = 'finished';
+		} else if (number[0] > 0) {
+			this.#element.rows[index].className = 'started';
+		}
 	}
 }
