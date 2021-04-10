767e4642de16cdfcb5c72b4342498da4b0f0e676
FrancoisBasset <>
2021-04-10 15:44:56 +0200

    maj

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const classes = require('../classes');
 
-router.get('/:verb', function(req, res) {
+router.get('/jetuilnousvousils/:verb', function(req, res) {
 	const json = classes.conjugateVerb(req.params.verb);
 
 	if (json == null) {
